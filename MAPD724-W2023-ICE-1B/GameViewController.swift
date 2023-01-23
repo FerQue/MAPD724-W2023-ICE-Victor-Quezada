59f5079356bff666715b2d0466a1c6277845ade6
FerQue <fernando_quezada1785@outlook.com>
2023-01-22 23:39:58 -0500

    Example in Class

---
+++ MAPD724-W2023-ICE-1B/GameViewController.swift
@@ -0,0 +1,36 @@
+//Example in class, create a SpriteKit project
+//Victor Quezada - 301286477
+//Follow the example in class
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        if let scene = GKScene(fileNamed: "GameScene")
+        {
+            if let sceneNode = scene.rootNode as! GameScene?
+            {
+                sceneNode.scaleMode = .aspectFill
+                if let view = self.view as! SKView?
+                {
+                    view.presentScene(sceneNode)
+                    view.ignoresSiblingOrder = true
+                }
+            }
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+    {
+        return .portrait
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
